9bcbced264693ad0a92f1aeac6b127c6d0e8fa5c
sartha117 <56217371+sartha117@users.noreply.github.com>
2020-07-10 23:41:04 +0530

    added fetch command

--- index.php
+++ index.php
@@ -10,6 +10,20 @@
 
   <body>
   <?php require_once 'process.php'; ?>
+  <?php
+    $mysqli =  new mysqli('localhost','root','','crud') or die(mysqli_error($mysqli));
+    $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
+    //pre_r($result);
+    pre_r($result->fetch_assoc());
+    pre_r($result->fetch_assoc());
+    pre_r($result->fetch_assoc());
+    function pre_r($array){
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+    }
+
+    ?>
   <div class="row justify-content-center">
     <form action="process.php" method="POST">
         <div class="form-group">
